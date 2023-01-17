7ee505657d141ebccb40cdae98910bf30efcd3d1
KJM <kjm99d@naver.com>
2023-01-17 22:05:21 +0900

    REFACTOR: Non-Blocking Socket Class Wrapper

--- CLIServer/main.cpp
+++ CLIServer/main.cpp
@@ -1,22 +1,8 @@
-﻿#ifdef UNICODE
-	#undef UNICODE
-#endif
+﻿
 
-#ifdef _UNICODE
-	#undef _UNICODE
-#endif
-
-#define BUFSIZE 512
-
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <WinSock2.h>
-#pragma comment(lib, "ws2_32.lib")
+#include "NBSocketServer.h"
 
 #include <vector>
-
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <signal.h>
 
 void     INThandler(int);
@@ -40,59 +26,9 @@ int main(int argc, const char* argv[])
 
 
 	const long nPort = 5003;
-
-	// 윈속 초기화에 실패할 경우 종료
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf("WSAStartup Failure \n");
-		return -1;
-	}
-
-	// 리턴값을 저장시킬 변수
-	int ret_value = 0;
-
-	// socket() - tcp 소켓 생성
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock == INVALID_SOCKET) {
-		printf("Create TCP Socket Failure \n");
-		return -1;
-	}
-
-	// 논블로킹 소켓으로 전환
-	u_long sock_on = 1;
-	ret_value = ioctlsocket(listen_sock, FIONBIO, &sock_on);
-	if (ret_value == SOCKET_ERROR) {
-		printf("ioctlsocket() Failure \n");
-		return -1;
-	}
-
-	// SO_REUSEADDR 소켓 옵션 설정(REUSEADDR)
-	BOOL optReuse = TRUE;
-	ret_value = setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (char*)&optReuse, sizeof(optReuse));
-	if (ret_value == SOCKET_ERROR) {
-		printf("setsockopt() REUSEADDR Failure \n");
-	}
-
-	// bind() - 서버의 지역 IP주소와 지역포트 번호를 위한 구조체 전달
-	SOCKADDR_IN serveraddr;
-	ZeroMemory(&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_port = htons(nPort);
-	serveraddr.sin_addr.S_un.S_addr = htons(INADDR_ANY);
-	ret_value = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
-	if (ret_value == SOCKET_ERROR) {
-		printf("bind() Failure \n");
-		return -1;
-	}
-
-
-	// listen() - bind에 의해 처리된 tcp 포트의 상태정보를 LISTEN으로 바꾼다.
-	ret_value = listen(listen_sock, SOMAXCONN);
-	if (ret_value == SOCKET_ERROR) {
-		printf("listen() Failure \n");
-		return -1;
-	}
+	CNBSocketServer server;
+	server.SetPort(nPort);
+	server.Bind();
 
 
 	// ===============================================================================
@@ -106,8 +42,9 @@ int main(int argc, const char* argv[])
 	while (keepRunning)
 	{ // 프로그램 대기상태를 위한 #while_1
 		// 패킷을 수신한다
+		SOCKET Sock = server.GetListen();
 		RecvPacket(sockets);
-		AddClient(sockets, listen_sock);
+		AddClient(sockets, Sock);
 
 		counter++;
 		if (counter % 10000 == 0)
@@ -126,11 +63,7 @@ int main(int argc, const char* argv[])
 	} // END #while_1
 
 	// ===============================================================================
-	printf("Exit ! \n");
-	closesocket(listen_sock);
-
-	// 윈속 종료
-	WSACleanup();
+	server.Clear();
 
 	return 0;
 }
