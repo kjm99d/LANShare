97af9f6d59b29554684286f96514ca5bd834a773
KJM <kjm99d@naver.com>
2023-01-30 15:09:23 +0900

    FIXED: 불필요한 코드 제거

--- CLIClient/main.cpp
+++ CLIClient/main.cpp
@@ -59,83 +59,6 @@ int main(int argc, const char* argv[])
 		if (read_size < 0) continue;
 
 
-		// 일단 보내자 
-#if 0
-		if (length > 0)
-		{
-			continue;
-			const int* cmd = (int*)buffer;
-			switch (*cmd)
-			{
-			case PROTOCOL_ID_CREATEFILE:
-			{
-
-				int FileSize = *(int*)(buffer + 4);
-				int kFs = FileSize;
-				//memset(buffer, 0x00, 512);
-				printf("CreateFile Length = [%d] [%d] ", FileSize, kFs);
-
-				while (true)
-				{
-					const int ReadSize = recv(client, (char*)buffer, 260, 0);
-					if (FileSize == 0)
-						break;
-
-					if (FileSize - ReadSize >= 0)
-					{
-						FileSize -= ReadSize;
-					}
-					else
-					{
-						FileSize -= FileSize;
-					}
-
-				}
-				char* m = (char*)malloc(kFs + 1);
-				memset(m, 0x00, kFs + 1);
-				memcpy(m, buffer, kFs);
-				printf("[%s] \n", m);
-
-				fopen_s(&fd, (char*)m, "wb");
-				delete m;
-			}
-			break;
-			case PROTOCOL_ID_WRITEFILE:
-			{
-				int FileSize = *(int*)(buffer + 4);
-				printf("CreateFile Length = [%d] \n", FileSize);
-				memset(buffer, 0x00, 512);
-
-				while (true)
-				{
-					const int ReadSize = recv(client, (char*)buffer, 260, 0);
-					if (FileSize == 0)
-						break;
-
-					if (FileSize - ReadSize >= 0)
-					{
-						fwrite(buffer, 1, length, fd);
-						FileSize -= ReadSize;
-					}
-					else
-					{
-						fwrite(buffer, 1, FileSize, fd);
-						FileSize -= FileSize;
-					}
-
-				}
-			}
-			break;
-			case PROTOCOL_ID_CLOSEHANDLE:
-				printf("CloseHandle \n");
-				fclose(fd);
-				fd = nullptr;
-				break;
-			}
-		}
-#endif
-
-
 		const int pkt_size = *(int*)buffer;
 		const int Cmd = *(int *)(buffer + 4);
 
@@ -148,7 +71,7 @@ int main(int argc, const char* argv[])
 				int tmp_read_size = recv(client, (char*)fileName, pkt_size - 4, 0);
 				if (tmp_read_size != (pkt_size - 4)) continue;
 
-				printf("CreateFile = %s \n", fileName);
+				printf(">> 파일 생성 :: %s \n", fileName);
 				fopen_s(&fd, fileName, "wb");
 				break;
 			}
@@ -174,7 +97,7 @@ int main(int argc, const char* argv[])
 		}
 		else if (Cmd == PROTOCOL_ID_CLOSEHANDLE)
 		{
-			printf("파일 다 씀 !");
+			printf(">> 파일 다운로드 완료");
 			
 			//char* file_data = (char*)malloc(pkt_size - 4);
 			//int tmp_read_size = recv(client, (char*)file_data, pkt_size - 4, 0);
