7e08c845a767891d16c52842dd787d9260d2e67c
KJM <kjm99d@naver.com>
2023-02-01 17:49:47 +0900

    FIXED: 일부코드 클래스화

--- CLIServer/HTTPServer.h
+++ CLIServer/HTTPServer.h
@@ -24,6 +24,7 @@ public:
 	void Receive(fp_HTTPEvent fp);
 
 private:
+	bool GetRequestPacket(SOCKET& ref_socket, std::string& ref);
 	bool Parse(const string& data, RequestHeader& ref);
 	bool Split(vector<string>& ref, string src, std::string delimeter);
 };
